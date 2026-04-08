c95e634198c54a8e00a448ac5dab58b57b1e2a02
Gourav Ghoshi <gouravghoshi255@gmail.com>
2026-04-08 16:20:14 +0530

    cors added in index.js

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ const PORT = process.env.PORT || 5000;
 // ============================================
 // Middleware
 // ============================================
-app.use(cors());
+app.use(cors({
+  origin: "https://vrn-sales.vercel.app",
+  credentials: true,
+}));
 app.use(express.json());
 
 // ============================================
